db664fac511e7bc014f9f425a3941beea4c76320
Melanie Richards <mr@melanie-richards.com>
2017-06-16 03:15:06 -0700

    Add clients page content

---
+++ wp-content/themes/mediaplus-2017/page-clients.php
@@ -0,0 +1,84 @@
+<?php get_header(); ?>
+<?php
+  while (have_posts()): the_post(); global $post;
+  $inpage_title = get_field('in-page_title');
+  $offerings_title = get_field('offerings_title');
+  $process_title = get_field('process_title');
+  $expertise_title = get_field('expertise_title');
+?>
+  <h1><?php echo $inpage_title; ?></h1>
+  <section>
+    <?php
+      $args = array(
+        'post_type' => 'offerings',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'order' => 'menu_order',
+      );
+      $items = get_posts($args);
+    ?>
+    <h2><?php echo $offerings_title; ?></h2>
+    <ul>
+      <?php foreach ($items as $item): ?>
+        <li><?php echo get_the_title($item->ID); ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </section>
+  <section>
+    <?php
+      $args = array(
+        'post_type' => 'process',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'order' => 'menu_order',
+      );
+      $items = get_posts($args);
+    ?>
+    <h2><?php echo $process_title; ?></h2>
+    <ul>
+      <?php foreach ($items as $item): ?>
+        <li><?php echo get_the_title($item->ID); ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </section>
+  <section>
+    <?php
+      $args = array(
+        'post_type' => 'expertise',
+        'post_status' => 'publish',
+        'posts_per_page' => -1,
+        'order' => 'menu_order',
+      );
+      $items = get_posts($args);
+    ?>
+    <h2><?php echo $expertise_title; ?></h2>
+    <ul>
+      <?php foreach ($items as $item): ?>
+        <li><?php echo get_the_title($item->ID); ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </section>
+<?php endwhile;?>
+
+<section aria-label="Client list">
+  <?php
+    $args = array(
+      'post_type' => 'clientele',
+      'post_status' => 'publish',
+      'posts_per_page' => -1,
+      'order' => 'menu_order',
+    );
+    $items = get_posts($args);
+  ?>
+  <ul class="clients">
+    <?php foreach ($items as $item): ?>
+      <li class="client">
+        <?php echo get_the_title($item->ID); ?>
+        <button>Expand details</button>
+        <?php echo get_field('dates_of_service', $item->ID); ?>
+      </li>
+    <?php endforeach; ?>
+  </ul>
+</section>
+
+<?php get_footer(); ?>
