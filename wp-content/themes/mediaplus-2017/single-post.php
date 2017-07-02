69ed8cbb740d01d92a0ee0133e38073556fa0ff6
Melanie Richards <mr@melanie-richards.com>
2017-07-02 13:27:36 -0700

    Style journal post typography

--- wp-content/themes/mediaplus-2017/single-post.php
+++ wp-content/themes/mediaplus-2017/single-post.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
 <?php while (have_posts()): the_post(); global $post; ?>
-  <?php the_title(); ?>
-  <?php the_content(); ?>
+  <div class="contain row">
+    <article class="post-content">
+      <h1 class="h2 post-content__title"><?php the_title(); ?></h1>
+      <?php the_content(); ?>
+    </article>
+  </div>
 <?php endwhile;?>
 <?php get_footer(); ?>
