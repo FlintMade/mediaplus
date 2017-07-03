4b1c04d602858a2d110e622afd72b7b56657e822
Melanie Richards <mr@melanie-richards.com>
2017-07-02 17:20:40 -0700

    Add banner image to individual journal posts

--- wp-content/themes/mediaplus-2017/single-post.php
+++ wp-content/themes/mediaplus-2017/single-post.php
@@ -1,10 +1,32 @@
 <?php get_header(); ?>
 <?php while (have_posts()): the_post(); global $post; ?>
-  <div class="contain row">
-    <article class="post-content">
-      <h1 class="h2 post-content__title"><?php the_title(); ?></h1>
-      <?php the_content(); ?>
-    </article>
-  </div>
+
+  <!-- BANNER IMAGE -->
+  <?php
+    $large = get_the_post_thumbnail_url('', 'large');
+    $xl = get_the_post_thumbnail_url('', 'mediaplus-xl');
+    $xxl = get_the_post_thumbnail_url('', 'mediaplus-xxl');
+    $xxxl = get_the_post_thumbnail_url('', 'mediaplus-xxxl');
+
+    if ($large):
+  ?>
+    <div class="page-section page-banner">
+      <picture>
+        <?php if ($xxl): ?>
+          <source media="(min-width: 1200px)" srcset="<?php echo $xxl; ?>">
+        <?php endif; ?>
+        <?php if ($xl): ?>
+          <source media="(min-width: 800px)" srcset="<?php echo $xl; ?>">
+        <?php endif; ?>
+        <img src="<?php echo $large; ?>" alt="" role="none" />
+      </picture>
+    </div>
+  <?php endif; ?>
+
+  <!-- POST CONTENT -->
+  <article class="page-section contain row post-content">
+    <h1 class="h2 post-content__title"><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+  </article>
 <?php endwhile;?>
 <?php get_footer(); ?>
