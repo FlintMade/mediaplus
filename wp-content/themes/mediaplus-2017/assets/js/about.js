113063a44b9e9b18b7eaf7c72360a025563fb38b
Melanie Richards <mr@melanie-richards.com>
2017-07-11 08:56:52 -0700

    Update person bio show/hide animation
    Resolves #52

--- wp-content/themes/mediaplus-2017/assets/js/about.js
+++ wp-content/themes/mediaplus-2017/assets/js/about.js
@@ -13,31 +13,96 @@
    */
 
   // Person toggle functionality
+
   var toggleBio = function(e) {
     var btn = e.currentTarget,
         person = findAncestor(btn, 'person'),
         personName = btn.getAttribute('data-name'),
+        initialInfo = person.querySelector('.person__initial'),
         bio = person.querySelector('.person__more');
 
-    // Hide bio
     if (btn.getAttribute('aria-expanded') === 'true') {
-      btn.setAttribute('aria-expanded', 'false');
-      btn.innerHTML = '<span aria-label="View more info about ' + personName + '">More info+</span>';
-
-      bio.setAttribute('aria-hidden', 'true');
-      person.classList.remove('active');
-
-    // Show bio
+      hideBio(btn, person, personName, initialInfo, bio);
     } else {
-      btn.setAttribute('aria-expanded', 'true');
-      btn.innerHTML = '<span aria-label="Close ' + personName + '’s info">-Close info</span>';
+      // First hide any other bios that might be shown
+      var shownBios = document.querySelectorAll('.person__more:not([aria-hidden="true"])');
+      for (var i = 0; i < shownBios.length; i++) {
+        var shownBio = shownBios[i],
+            shownPerson = findAncestor(shownBio, 'person'),
+            shownBtn = shownPerson.querySelector('.person__toggle'),
+            shownPersonName = shownBtn.getAttribute('data-name'),
+            shownInitialInfo = shownPerson.querySelector('.person__initial');
+        hideBio(shownBtn, shownPerson, shownPersonName, shownInitialInfo, shownBio);
+      }
 
-      person.classList.add('active');
-
-      setTimeout(function(){
-        bio.removeAttribute('aria-hidden');
-      }, 100);
+      // Show new one after shown bios are halfway closed
+      if (shownBios.length > 0) {
+        setTimeout(function(){
+          showBio(btn, person, personName, initialInfo, bio);
+        }, 800);
+      } else {
+        showBio(btn, person, personName, initialInfo, bio);
+      }
     }
+  };
+
+  // Hide bio
+  var hideBio = function(btn, person, personName, initialInfo, bio) {
+    // Slide :after back over text
+    person.classList.remove('fadedIn');
+
+    // After the :after slides back in, remove it and slide :before
+    setTimeout(function() {
+      // Hide bio and initial
+      initialInfo.style.opacity = '0';
+      bio.setAttribute('aria-hidden', 'true');
+
+      person.classList.remove('blockedBio');
+      person.classList.remove('slidUp');
+      
+      // Timeout allows for .person:before CSS transition
+      setTimeout(function() {
+        // Fade in initial info
+        fade(initialInfo, 0, 1, 200);
+
+        // Toggle button state
+        btn.setAttribute('aria-expanded', 'false');
+        btn.innerHTML = '<span aria-label="View more info about ' + personName + '’s info">More Info+</span>';
+      }, 300);
+    }, 1200);
+  };
+
+  // Show bio
+  var showBio = function(btn, person, personName, initialInfo, bio) {
+    // Fade out initial info
+    fade(initialInfo, 1, 0, 200);
+
+    // Toggle button state
+    btn.setAttribute('aria-expanded', 'true');
+    btn.innerHTML = '<span aria-label="Close ' + personName + '’s info">-Close Info</span>';
+
+    // Timeout allows for initialInfo CSS transition
+    setTimeout(function() {
+
+      // Slide up overlay :before pseudo-el
+      person.classList.add('slidUp');
+
+      // Timeout allows for .person:before CSS transition
+      setTimeout(function() {
+
+        // Show :after
+        person.classList.add('blockedBio');
+
+        setTimeout(function() {
+          // Slide out that :after
+          person.classList.add('fadedIn');
+
+          // Show bio and initial info
+          initialInfo.style.opacity = '1';
+          bio.removeAttribute('aria-hidden');
+        }, 200);
+      }, 300);
+    }, 300);
   };
 
   // ON LOAD
