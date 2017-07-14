37d632639e317f696120cc7771e345b5aee17e59
Melanie Richards <mr@melanie-richards.com>
2017-07-14 09:01:38 -0700

    Style journal images
    Resolves #25

--- wp-content/themes/mediaplus-2017/functions.php
+++ wp-content/themes/mediaplus-2017/functions.php
@@ -268,6 +268,117 @@ function filter_ptags_on_images($content) {
 add_filter('the_content', 'filter_ptags_on_images');
 
 /*
+ *	SET UP OWN POST GALLERY MARKUP
+ *	-----------------------------------------------------------------
+ */
+
+remove_shortcode('gallery', 'gallery_shortcode');
+
+function mediaplus_gallery_shortcode($attr) {
+	$post = get_post();
+
+	static $instance = 0;
+	$instance++;
+
+	if ( ! empty( $attr['ids'] ) ) {
+			// 'ids' is explicitly ordered, unless you specify otherwise.
+			if ( empty( $attr['orderby'] ) ) {
+					$attr['orderby'] = 'post__in';
+			}
+			$attr['include'] = $attr['ids'];
+	}
+
+	/**
+		* Filters the default gallery shortcode output.
+		*
+		* If the filtered output isn't empty, it will be used instead of generating
+		* the default gallery template.
+		*
+		* @since 2.5.0
+		* @since 4.2.0 The `$instance` parameter was added.
+		*
+		* @see gallery_shortcode()
+		*
+		* @param string $output   The gallery output. Default empty.
+		* @param array  $attr     Attributes of the gallery shortcode.
+		* @param int    $instance Unique numeric ID of this gallery shortcode instance.
+		*/
+	$output = apply_filters( 'post_gallery', '', $attr, $instance );
+	if ( $output != '' ) {
+			return $output;
+	}
+
+	$html5 = current_theme_supports( 'html5', 'gallery' );
+	$atts = shortcode_atts( array(
+			'order'      => 'ASC',
+			'orderby'    => 'menu_order ID',
+			'id'         => $post ? $post->ID : 0,
+			'include'    => '',
+			'exclude'    => '',
+			'link'       => ''
+	), $attr, 'gallery' );
+
+	$id = intval( $atts['id'] );
+
+	if ( ! empty( $atts['include'] ) ) {
+			$_attachments = get_posts( array( 'include' => $atts['include'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
+
+			$attachments = array();
+			foreach ( $_attachments as $key => $val ) {
+					$attachments[$val->ID] = $_attachments[$key];
+			}
+	} elseif ( ! empty( $atts['exclude'] ) ) {
+			$attachments = get_children( array( 'post_parent' => $id, 'exclude' => $atts['exclude'], 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
+	} else {
+			$attachments = get_children( array( 'post_parent' => $id, 'post_status' => 'inherit', 'post_type' => 'attachment', 'post_mime_type' => 'image', 'order' => $atts['order'], 'orderby' => $atts['orderby'] ) );
+	}
+
+	if ( empty( $attachments ) ) {
+			return '';
+	}
+
+	$selector = "gallery-{$instance}";
+	$gallery_div = "<div id='$selector' class='post__gallery'>";
+
+	/**
+		* Filters the default gallery shortcode CSS styles.
+		*
+		* @since 2.5.0
+		*
+		* @param string $gallery_style Default CSS styles and opening HTML div container
+		*                              for the gallery shortcode output.
+		*/
+	$output = apply_filters( 'gallery_style', $gallery_style . $gallery_div );
+
+	$i = 0;
+	foreach ( $attachments as $id => $attachment ) {
+			$image_output = wp_get_attachment_image( $id, $atts['size'], false, $attr );
+			$image_meta  = wp_get_attachment_metadata( $id );
+
+			$orientation = '';
+			if ( isset( $image_meta['height'], $image_meta['width'] ) ) {
+					$orientation = ( $image_meta['height'] > $image_meta['width'] ) ? 'portrait' : 'landscape';
+			}
+			$output .= "<div class='gallery-item'>";
+			$output .= "$image_output";
+			if (trim($attachment->post_excerpt) ) {
+					$output .= "
+							<p class='wp-caption-text gallery-caption' id='$selector-$id'>
+							" . wptexturize($attachment->post_excerpt) . "
+							</p>";
+			}
+			$output .= "</div>";
+	}
+
+	$output .= "
+			</div>\n";
+
+	return $output;
+}
+
+add_shortcode('gallery', 'mediaplus_gallery_shortcode');
+
+/*
  *	FLUSH! THOSE! BUFFERS!
  *	And fix a PHP error
  *	-----------------------------------------------------------------
