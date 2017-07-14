8fa23225862a4068a1bfc68df3a6c6e40ab1b94b
Melanie Richards <mr@melanie-richards.com>
2017-07-13 18:13:42 -0700

    Don't load MC script on flow pages

--- wp-content/themes/mediaplus-2017/footer.php
+++ wp-content/themes/mediaplus-2017/footer.php
@@ -61,8 +61,10 @@
 <script src="https://use.typekit.net/iuq2xoe.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
 
-<!-- MAILCHIMP -->
-<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!-- MAILCHIMP SCRIPT -->
+<?php if (!is_front_page() && !is_post_type_archive('expertise') && !is_singular('expertise') && !is_post_type_archive('offerings') && !is_singular('offerings')): ?>
+  <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<?php endif ;?>
 
 <?php if (is_singular('post')): ?>
   <script>
