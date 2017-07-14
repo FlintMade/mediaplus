a3d6097590d27e7e91c8ce354cc783c500ae04b2
Melanie Richards <mr@melanie-richards.com>
2017-07-13 18:01:44 -0700

    Moving blocking scripts to end of body

--- wp-content/themes/mediaplus-2017/footer.php
+++ wp-content/themes/mediaplus-2017/footer.php
@@ -58,6 +58,12 @@
 
 <?php wp_footer(); ?>
 
+<script src="https://use.typekit.net/iuq2xoe.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+<!-- MAILCHIMP -->
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[3]='BIRTHDAY';ftypes[3]='birthday';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+
 <?php if (is_singular('post')): ?>
   <script>
     fluidvids.init({
