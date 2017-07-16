cc296c65c69ceae2070c477c54dd6c0fdfdeb6ad
Melanie Richards <mr@melanie-richards.com>
2017-07-15 18:20:35 -0700

    Add 404 page
    Resolves #99

--- wp-content/themes/mediaplus-2017/404.php
+++ wp-content/themes/mediaplus-2017/404.php
@@ -1,3 +1,6 @@
 <?php get_header(); ?>
 
+<h1>Page not found</h1>
+<p>Sorry, we couldn’t find that page! <a href="<?php echo get_bloginfo('url'); ?>">Start at the beginning?</a></p>
+
 <?php get_footer(); ?>
