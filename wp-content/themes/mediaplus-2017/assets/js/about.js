deecc329cc160281d10c2fbe7671c4d8c1e1a5f2
Melanie Richards <mr@melanie-richards.com>
2017-07-26 17:35:40 -0700

    Move name to the top of people blocks

--- wp-content/themes/mediaplus-2017/assets/js/about.js
+++ wp-content/themes/mediaplus-2017/assets/js/about.js
@@ -18,11 +18,10 @@
     var btn = e.currentTarget,
         person = findAncestor(btn, 'person'),
         personName = btn.getAttribute('data-name'),
-        initialInfo = person.querySelector('.person__initial'),
         bio = person.querySelector('.person__more');
 
     if (btn.getAttribute('aria-expanded') === 'true') {
-      hideBio(btn, person, personName, initialInfo, bio);
+      hideBio(btn, person, personName, bio);
     } else {
       // First hide any other bios that might be shown
       var shownBios = document.querySelectorAll('.person__more:not([aria-hidden="true"])');
@@ -30,26 +29,24 @@
         var shownBio = shownBios[i],
             shownPerson = findAncestor(shownBio, 'person'),
             shownBtn = shownPerson.querySelector('.person__toggle'),
-            shownPersonName = shownBtn.getAttribute('data-name'),
-            shownInitialInfo = shownPerson.querySelector('.person__initial');
-        hideBio(shownBtn, shownPerson, shownPersonName, shownInitialInfo, shownBio);
+            shownPersonName = shownBtn.getAttribute('data-name');
+        hideBio(shownBtn, shownPerson, shownPersonName, shownBio);
       }
 
       // Show new one after shown bios are halfway closed
       if (shownBios.length > 0) {
         setTimeout(function(){
-          showBio(btn, person, personName, initialInfo, bio);
+          showBio(btn, person, personName, bio);
         }, 450);
       } else {
-        showBio(btn, person, personName, initialInfo, bio);
+        showBio(btn, person, personName, bio);
       }
     }
   };
 
   // Hide bio
-  var hideBio = function(btn, person, personName, initialInfo, bio) {
+  var hideBio = function(btn, person, personName, bio) {
     // Hide bio and initial
-    initialInfo.style.opacity = '0';
     bio.setAttribute('aria-hidden', 'true');
 
     // Timeout allows for bio CSS transition
@@ -58,9 +55,6 @@
       
       // Timeout allows for .person:before CSS transition
       setTimeout(function() {
-        // Fade in initial info
-        initialInfo.style.opacity = '1';
-
         // Toggle button state
         btn.setAttribute('aria-expanded', 'false');
         btn.innerHTML = '<span aria-label="View more info about ' + personName + '’s info">More Info+</span>';
@@ -69,27 +63,19 @@
   };
 
   // Show bio
-  var showBio = function(btn, person, personName, initialInfo, bio) {
-    // Fade out initial info
-    initialInfo.style.opacity = '0';
-
+  var showBio = function(btn, person, personName, bio) {
     // Toggle button state
     btn.setAttribute('aria-expanded', 'true');
     btn.innerHTML = '<span aria-label="Close ' + personName + '’s info">-Close Info</span>';
 
-    // Timeout allows for initialInfo CSS transition
+    // Slide up overlay :before pseudo-el
+    person.classList.add('slidUp');
+
+    // Timeout allows for .person:before CSS transition
     setTimeout(function() {
-
-      // Slide up overlay :before pseudo-el
-      person.classList.add('slidUp');
-
-      // Timeout allows for .person:before CSS transition
-      setTimeout(function() {
-        // Show bio and initial info
-        initialInfo.style.opacity = '1';
-        bio.removeAttribute('aria-hidden');
-      }, 500);
-    }, 300);
+      // Show bio and initial info
+      bio.removeAttribute('aria-hidden');
+    }, 500);
   };
 
   // ON LOAD
